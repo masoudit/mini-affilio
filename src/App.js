d84d9aae6fd02745a4cdb4e7d7a729af4c95c66b
masoud.it <masoud.it2012@gmail.com>
2022-02-13 17:19:55 +0330

    INIT

--- src/App.js
+++ src/App.js
@@ -1,57 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.less';
+import 'antd/dist/antd.less'
+// import 'antd/dist/antd.css'
+import { Alert, DatePicker, ConfigProvider } from 'antd';
+import locale from 'antd/lib/locale/fa_IR';
+import moment from 'moment-jalaali';
+import { DatePicker as DatePickerJalali } from "antd-jalali";
+const { RangePicker } = DatePickerJalali;
+
+
+moment.locale("fa");
+
 
 function App() {
+  const handleChange = () => {}
   return (
+    <ConfigProvider locale={locale} >
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker  onChange={handleChange} />
+      <DatePickerJalali  onChange={handleChange} />
+      <RangePicker />
+      <Alert message="Selected Date" description={"test"} />
+      <div style={{ marginTop: 16 }}>
+        {/* Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'} */}
+      </div>
     </div>
+    </div>
+    </ConfigProvider>
   );
 }
 
