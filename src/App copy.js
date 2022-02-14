4aaead6f817489a678e51c25b6811027588215d2
masoud.it <masoud.it2012@gmail.com>
2022-02-14 14:12:59 +0330

    Ant Design setup

--- src/App copy.js
+++ src/App copy.js
@@ -3,23 +3,32 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.less';
 import 'antd/dist/antd.less'
-import 'antd/dist/antd.css'
+// import 'antd/dist/antd.css'
 import { Alert, DatePicker, ConfigProvider } from 'antd';
 import locale from 'antd/lib/locale/fa_IR';
 import moment from 'moment-jalaali';
 import { DatePicker as DatePickerJalali } from "antd-jalali";
-import Layout_ from './app/Layout';
-// const { RangePicker } = DatePickerJalali;
+const { RangePicker } = DatePickerJalali;
+
 
 moment.locale("fa");
+
 
 function App() {
   const handleChange = () => {}
   return (
     <ConfigProvider locale={locale} >
-      <Layout_ >
-        xxx
-      </Layout_>
+    <div className="App">
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker  onChange={handleChange} />
+      <DatePickerJalali  onChange={handleChange} />
+      <RangePicker />
+      <Alert message="Selected Date" description={"test"} />
+      <div style={{ marginTop: 16 }}>
+        {/* Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'} */}
+      </div>
+    </div>
+    </div>
     </ConfigProvider>
   );
 }
